c34bd1b1008a7c69c881020ae6d7209755a2f25a
CLD <cld.username@gmail.com>
2025-09-12 07:55:49 +0000

    Fix init_project.sh

--- tools/canmv/init_project.sh
+++ tools/canmv/init_project.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "初始化 repo 工作区..."
-repo init -u https://github.com/hexchip/k230_rtos_sdk_manifest.git -b main
+repo init -u https://github.com/hexchip/canmv-k230-manifest.git -b main
 
 if [ -d ".git" ]; then
+    mkdir -p .repo/project-objects/hexchip
     mv .git .repo/project-objects/hexchip/k230_rtos_sdk.git
 fi
 
