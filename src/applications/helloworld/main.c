4035c33a648be01369a0220951ef617d7184bfcc
CLD <cld.username@gmail.com>
2025-10-31 10:12:46 +0000

    tmp

--- src/applications/helloworld/main.c
+++ src/applications/helloworld/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  printf("hello world.\n");
+#include <display_manager.h>
 
+#include <rtthread.h>
+
+int main(int argc, char *argv[]) {
+  printf("hello world1.\n");
+
+  display_manager_access_display();
+
+  printf("hello world2.\n");
+
+  rt_thread_mdelay(5000);
+
+  display_manager_leave_display();
+
+  printf("hello world3.\n");
   return 0;
 }
